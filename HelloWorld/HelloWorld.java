ca22298aa7bd43006dbd81cc6e075c900c5691f9
E.Moraes <emmanoelnascimento37@gmail.com>
2024-10-11 12:49:50 -0300

    Criada pasta HelloWorld com programa inicial do curso

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,20 @@
+// HelloWorld.exe
+// Início dos exercícios baseados em java e advindos do curso do professor Nélio Alves na Udemy
+// Este programa exibe uma breve mensagem de texto no prompt
+
+
+
+public class HelloWorld {
+
+
+    // inicia  execução do programa
+    public static void main (String[] args) {
+
+        // exibe mensagem  no prompt
+        System.out.println("Hello World, this is the beginning of the course!");
+    } // fim da execução do programa
+
+} // fim da classe HelloWorld
+
+
+
