4bff8bdd4d47e35676e93585a68f295adfdd8a04
E.Moraes <emmanoelnascimento37@gmail.com>
2024-10-15 13:17:08 -0300

    Versão alternativa, utilizando Locale.setDefault(Locale.US)

--- UnidadeQuatro/ModificaPadraoDeExibicaoInsercaoDeDados.java
+++ UnidadeQuatro/ModificaPadraoDeExibicaoInsercaoDeDados.java
@@ -13,11 +13,12 @@ public class ModificaPadraoDeExibicaoInsercaoDeDados {
     // inicio da execução do programa
     public static void main(String [] args){
 
+        //Habilita padrão US no programa
+        Locale.setDefault(Locale.US);
 
         // cria objeto Scanner e habilita a entrada de dados via janela prompt
         Scanner sc = new Scanner(System.in);
-        // Define o locale do Scanner
-        sc.useLocale(Locale.US);
+
 
         double numero; // variável numero é criada
 
@@ -29,10 +30,9 @@ public class ModificaPadraoDeExibicaoInsercaoDeDados {
         // exibe o número no padrão dos Estados Unidos
         System.out.println("O valor inserido de acordo com o padrão US foi: " + numero);
 
-
         sc.close(); // encerra objeto Scanner
 
-
+        
     } // fim da main
 
 } // fim da classe ModificaPadraoDeExibicaoInsercaoDeDados
