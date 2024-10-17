aa618502a3bad2dec76af68f3b836c12bff5cd7d
E.Moraes <emmanoelnascimento37@gmail.com>
2024-10-17 12:03:34 -0300

    Exibe os principais operadores comparativos

---
+++ UnidadeCinco/OperadoresComparativos.java
@@ -0,0 +1,21 @@
+// OperadoresComparativos.java
+
+//  Exibe os principais operadores  comparativos em java
+
+
+public class OperadoresComparativos {
+
+    // Inicia execução do programa
+    public static void main (String [] args){
+
+
+        System.out.println("Operador Maior: >\nEx. 8 > 2" + " resultado: " + (8 > 2) + "\n");
+        System.out.println("Operador Menor: <\nEx. 8 < 2" + " resultado: " + (8 < 2) + "\n");
+        System.out.println("Operador Maior ou igual: >=\nEx. 8 >= 2" + " resultado: " + (8 >= 2) + "\n");
+        System.out.println("Operador Menor ou igual: <=\nEx. 8 <= 2" + " resultado: " + (8 <= 2) + "\n");
+        System.out.println("Operador igual: ==\nEx. 8 == 2" + " resultado: " + (8 == 2) + "\n");
+        System.out.println("Operador Diferente: !=\nEx. 8 != 2" + " resultado: " + (8 != 2) + "\n");
+
+    } // fim da main
+
+} // fim da classe OperadoresComparativos
