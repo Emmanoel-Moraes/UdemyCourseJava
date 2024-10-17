c77dad9b425cac46f73f86991b3e071bc17b4e76
E.Moraes <emmanoelnascimento37@gmail.com>
2024-10-17 12:36:06 -0300

    Manipula operadores lógicos E, OU e NÃO

---
+++ UnidadeCinco/OperadoresLogicos.java
@@ -0,0 +1,53 @@
+// OperadoresLogicos.java
+
+// Exibe os 3 operadres lógicos em java ( E, OU e NÃO)
+
+import java.util.Scanner;
+
+public class OperadoresLogicos {
+
+    // inicia execução do  programa
+    public static void  main (String[] args){
+
+        // habilita leitura de dados via janela de  comando
+        Scanner sc = new Scanner (System.in);
+
+
+        int x, y;
+
+        // prompt
+        System.out.println("Insira um inteiro para X: ");
+        x = sc.nextInt(); // x recebe valor inserido pelo usuário
+
+        // prompt
+        System.out.println("Insira um inteiro para Y: ");
+        y = sc.nextInt(); // y recebe valor inserido pelo usuário
+
+        System.out.println("\nOperador 'E' retorna valor verdadeiro se todas as condições forem satisfeitas\nExemplo: (x > y) E (y = 6) resultado: " + (x > y && y == 6));
+        System.out.println("\nOperador 'OU' retorna valor verdadeiro se ao menos uma das condições for satisfeita\nExemplo: (x > y) OU (y = 6) resultado: " + (x > y || y == 6));
+        System.out.println("\nOperador 'NÃO' retorna valor falso se as condições forem satisfeitas\nExemplo: (x não é maior que y) resultado: " + !(x > y) + "\n");
+
+
+        System.out.println("Tabela verdade para operador &&");
+        System.out.println("A && B  resultado:");
+        System.out.println("F && F  F");
+        System.out.println("F && V  F");
+        System.out.println("V && F  F");
+        System.out.println("V && V  V");
+
+        System.out.println("\nTabela verdade para operador ||");
+        System.out.println("A || B  resultado:");
+        System.out.println("F || F  F");
+        System.out.println("F || V  V");
+        System.out.println("V || F  V");
+        System.out.println("V || V  V");
+
+
+        System.out.println("\nTabela verdade para operador !");
+        System.out.println("!A  resultado:");
+        System.out.println("F   V");
+        System.out.println("V   F");
+
+    } // fim da main
+
+} // fim da classe OperadoresLogicos
