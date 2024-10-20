2eb6f3c19013c73160c4af249092d39271710e27
E.Moraes <emmanoelnascimento37@gmail.com>
2024-10-20 16:01:47 -0300

    Estruturas Repetitivas

---
+++ UnidadeSeis/EstruturaWhile.java
@@ -0,0 +1,27 @@
+// EstruturaWhile.java
+
+// Demontrando  funcionamento da estrura de repetição while
+
+
+import java.util.Scanner;
+
+
+public class EstruturaWhile {
+
+    // Inicia execução do programa
+    public static void  main (String[] args) {
+
+        // habilita leitura de dados via janela de comando
+        Scanner sc = new Scanner(System.in);
+
+        // Lê número inserido via prompt de comando e atribui a num
+        int num = sc.nextInt();
+
+        // Enquanto o número inserido não for zero
+        while (num != 0) {
+            num = sc.nextInt(); // Lê número atribui a num
+        }
+
+    } // fim da main
+
+} // fim da classe EstruturaWhile
