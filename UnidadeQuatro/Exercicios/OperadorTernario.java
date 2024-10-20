7db6a82ae7ca05e2f84751016f25d3c491b33f31
E.Moraes <emmanoelnascimento37@gmail.com>
2024-10-20 14:45:42 -0300

    Utilizando Locale.setDefault(Locale.US) para modificar  padrão de recebimento e exibição de dados

---
+++ UnidadeQuatro/Exercicios/OperadorTernario.java
@@ -0,0 +1,21 @@
+// OperadorTernario.java
+
+// Demonstra aplicação do operador ternário na resolução de um problema
+
+
+public class OperadorTernario {
+
+    // Inicia execuão do programa
+    public static void main (String[] args){
+
+        double preco = 34.5;
+        double desconto;
+
+                // condição ? valor se verdadeiro : valor se falso
+        desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+
+        System.out.println(desconto);
+
+    } // fim da main
+
+} // fim da classe OperadorTernario
