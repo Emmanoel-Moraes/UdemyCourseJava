50240e3db03d41afee085d9274355da8247ae7ab
E.Moraes <emmanoelnascimento37@gmail.com>
2024-10-20 13:24:51 -0300

    Utiliza operador ternário para verificar se número é negativo

--- UnidadeCinco/Exercicios/TestaNegativo.java
+++ UnidadeCinco/Exercicios/TestaNegativo.java
@@ -1,4 +1,25 @@
-package org.udemy.UnidadeCinco.Exercicios;
+//TestaNegativo.java
+
+//
+
+import java.util.Scanner; // Utiliza Scanner para receber dados via prompt
 
 public class TestaNegativo {
-}
+
+    // Inicia  execução do programa
+    public static void main (String[] args){
+
+        // Habilita leitura de dados via janela de comando
+        Scanner sc = new Scanner(System.in);
+
+
+        int inteiro = sc.nextInt(); // Lê um número e atribui a inteiro
+
+
+        System.out.println((inteiro < 0 ? "NEGATIVO" : "NÃO NEGATIVO"));
+
+        sc.close();
+
+    } // fim da main
+
+} // fim da classe TestaNegativo
