14602a20810362d1b08b7ec87f3b7cf22521c833
E.Moraes <emmanoelnascimento37@gmail.com>
2024-10-20 11:27:05 -0300

    Operadores matemáticos simplificados

--- UnidadeCinco/OperadoresDeAtribuicaoCumulativa.java
+++ UnidadeCinco/OperadoresDeAtribuicaoCumulativa.java
@@ -1,4 +1,44 @@
-package org.udemy.UnidadeCinco;
+//  OperadoresDeAtribuicao.java
+
+// Utiliza operador simplificado para iterar valor na variável
+
+/*
+
+Principais operadores acumuladores:
+
+a += b
+a -= b
+a *= b
+a /= b
+a %= b
+
+*/
+
+import java.util.Scanner; // Utiliza Scanner para ler dados via prompt
 
 public class OperadoresDeAtribuicaoCumulativa {
-}
+
+    // Inicia execução do programa
+    public static void main (String [] args){
+
+        // Habilita leitura de dados via janela de  comando
+        Scanner sc = new Scanner (System.in);
+
+
+        int minutos;
+        double conta = 50;
+
+        minutos = sc.nextInt(); // atribui valor inserido pelo usuário
+
+        // Se ultrapassar mais de 100 minutos de ligação
+        if (minutos > 100){
+            conta +=  (minutos - 100) * 2.0;
+        }
+        // Exibe conta
+        System.out.printf("Valor a pagar: R$ %.2f\n", conta);
+
+        sc.close(); // fecha objeto scanner
+
+    } // fim da main
+
+} // fim da classe OperadoresDeAtribuicao
