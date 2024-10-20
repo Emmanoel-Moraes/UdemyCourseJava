7cd2b1dbbd22efebded9b8a268b65554740b3c53
E.Moraes <emmanoelnascimento37@gmail.com>
2024-10-20 12:24:26 -0300

    Lê um número qualquer e informa a que intervalo pertence

--- UnidadeCinco/Exercicios/EncontraIntervalo.java
+++ UnidadeCinco/Exercicios/EncontraIntervalo.java
@@ -1,4 +1,42 @@
-package org.udemy.UnidadeCinco.Exercicios;
+// EncontraIntervalo.java
+
+// Lê um número qualquer e informa o intervalo que ele pertence. Caso não seja encontrado, exibe a mensagem "Fora de intervalo"
+
+
+// Utiliza Scanner
+import java.util.Scanner;
+
 
 public class EncontraIntervalo {
-}
+
+    // inicia execução do programa
+    public static void  main (String[] args ){
+
+        // Habilita recebimento de informações via janela de comando
+        Scanner sc = new Scanner (System.in);
+
+        // Lê um número qualquer o armazena na variável numero
+        double numero = sc.nextDouble();
+
+
+        if (numero >= 0 && numero <= 25)
+            System.out.println("Intervalo [0,25]");
+
+        else if (numero > 25 && numero <= 50)
+            System.out.println("Intervalo (25,50]");
+
+        else if (numero > 50 && numero <=75)
+            System.out.println("Intervalo (50,75]");
+
+        else if (numero > 75 && numero <= 100)
+            System.out.println("Intervalo (75,100]");
+
+        else
+            System.out.println("Fora de intervalo");
+
+
+        sc.close(); // encerra objeto Scanner
+
+    } // fim da main
+
+} // fim da classe EncontraIntervalo
