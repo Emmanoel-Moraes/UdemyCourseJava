54c1545bd689a6d86ed5a3541d7eed2500d655e1
E.Moraes <emmanoelnascimento37@gmail.com>
2024-10-20 23:45:33 -0300

    Utiliza while para verificar se senha inserida é compatível com a senha armazenada

--- UnidadeSeis/VerificaSenhaComWhile.java
+++ UnidadeSeis/VerificaSenhaComWhile.java
@@ -1,4 +1,31 @@
-package org.udemy.UnidadeSeis;
+// VerificaSenhaComWhile.java
+
+// Realiza leitura de uma senha até que seja válida, informando quando está incorreta
+
+
+import java.util.Scanner; // Utiliza Scanner
+
 
 public class VerificaSenhaComWhile {
-}
+
+    // Inicia execução do programa
+    public static void main (String [] args){
+
+        // Habilita leitura de dados via janela de comando
+        Scanner sc = new Scanner(System.in);
+
+        // Lê um inteiro e armazena em password
+        int password = sc.nextInt();
+
+        // Enquanto senha incorreta
+        while (password != 2002){
+            System.out.println("Senha inválida");
+            password = sc.nextInt();
+        }
+
+        //Senha correta digitada
+        System.out.println("Acesso permitido");
+
+    } // fim da main
+
+} // fim da classe
