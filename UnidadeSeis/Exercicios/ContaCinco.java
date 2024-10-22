6501dce3bd5bddfdd8e96952c12c445396a8e4df
E.Moraes <emmanoelnascimento37@gmail.com>
2024-10-22 00:42:09 -0300

    Exibe contagem de 1 a 5

--- UnidadeSeis/Exercicios/ContaCinco.java
+++ UnidadeSeis/Exercicios/ContaCinco.java
@@ -1,4 +1,19 @@
-package org.udemy.UnidadeSeis.Exercicios;
+// ContaCinco.java
+
+// Exibe contagem em 5 por meio de estrutura for
+
 
 public class ContaCinco {
-}
+
+    // inicia execução do programa
+    public static void main (String [] args){
+
+
+        for (int n = 5; n >= 1; n--){
+
+            System.out.println("Valor de n: " + n);
+        }
+
+    } // fim da main
+
+} // fim da classe ContaCinco
