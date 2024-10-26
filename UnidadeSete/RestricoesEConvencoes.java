05be52f5badc28d8bc162650c53f42506ae4e10c
E.Moraes <emmanoelnascimento37@gmail.com>
2024-10-25 23:23:11 -0300

    Sintaxe de variáveis, classes e funções

--- UnidadeSete/RestricoesEConvencoes.java
+++ UnidadeSete/RestricoesEConvencoes.java
@@ -1,13 +1,13 @@
 // RestricoesEConvencoes.java
 
-//
+// Demonstra formas possíveis e restritas para declarações de variáveis, classes e funções
 
 public class RestricoesEConvencoes {
 
     // Inicia execução do programa
     public static void main (String[] args) {
 
-
+        // Para variáveis
         // iniciar com letra ou underscore _ são formas válidas de declarar variáveis
         int formaValidaDeDeclararVariaveis;
         int _formaValidaDeDeclararVariaveis;
@@ -16,6 +16,13 @@ public class RestricoesEConvencoes {
         // Não iniciar variáveis com número ou símbolos
         // Ex. 5numero, $salario
 
+
+        // Para classes
+        // Iniciar sempre com letra Maiúscula,modo Pascal Case
+        // Ex. ExemploDeClasse
+
+        // Para funções
+        // Segue a regra de declaração de variáveis
 
     } // fim da main
 
