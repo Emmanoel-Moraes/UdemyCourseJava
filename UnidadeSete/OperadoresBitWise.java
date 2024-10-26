95822275f8c5684f31fd86155f9eb081086c60aa
E.Moraes <emmanoelnascimento37@gmail.com>
2024-10-26 00:15:35 -0300

    Sintaxe BitWise

--- UnidadeSete/OperadoresBitWise.java
+++ UnidadeSete/OperadoresBitWise.java
@@ -1,4 +1,22 @@
-package org.udemy.UnidadeSete;
+// OperadoresBitWise
+
+// Operadores bitwise são operadores para testes lógicos
+// E = &
+// OU = |
+// OU exclusivo = ^
+
 
 public class OperadoresBitWise {
-}
+
+    // Inicia execução do programa
+    public static void main (String[] args){
+
+        int a = 2, b = 4;
+
+        System.out.println(a & b);
+        System.out.println(a | b);
+        System.out.println(a ^ b);
+
+    } // fim da main
+
+} // fim da classe OperadoresBitWise
